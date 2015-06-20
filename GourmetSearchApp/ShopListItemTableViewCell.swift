a2e6d8258bc651d7646026b9eef180d93b61c38d
niixnoughtnothing <ryoh.t778@gmail.com>
2015-06-20 14:29:48 +0900

    Added new Class "ShopListItemTableViewCell"
    This class correspond to "Shop list Item cell in a table View
    (storyboard)”.

---
+++ GourmetSearchApp/ShopListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopListItemTableViewCell.swift
+//  GourmetSearchApp
+//
+//  Created by nixnoughtnothing on 6/20/15.
+//  Copyright (c) 2015 Sttir Inc. All rights reserved.
+//
+
+import UIKit
+
+class ShopListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
