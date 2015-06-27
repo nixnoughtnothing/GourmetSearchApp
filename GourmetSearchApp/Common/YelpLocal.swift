87e3b9fabb190c377bac8bb5a90f46469ed5d484
niixnoughtnothing <ryoh.t778@gmail.com>
2015-06-27 15:10:19 +0900

    added struct "Shop" in YelpLocal.swift
    YelpAPIから渡ってくる店舗情報を格納する変数を構造体”Shop”の中で定義
    Printableプロトコルを採用。(descriptionメソッドが実装されていると、Shop型のインスタンスを文字列として評価した際にdes
    criptionメソッドの返り値が表示される）→printlnした場合に店舗情報の中身が確認できるので、便利。

--- GourmetSearchApp/Common/YelpLocal.swift
+++ GourmetSearchApp/Common/YelpLocal.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+
+/* APIから渡ってきたJSON Dataのパースに使用するSwiftJSONの値がOptional型を返すので、
+   構造体Shop型のプロパティは基本的にすべてOptional型にする。こうすることでコード量を減らすことができる。
+   Printableプロトコルついて => http://nekogata.hatenablog.com/entry/2014/12/19/060122
+*/
+
+public struct Shop : Printable {
+    public var gid: String? = nil
+    public var name: String? = nil
+    public var photoUrl: String? = nil
+    public var yomi: String? = nil
+    public var tel: String? = nil
+    public var address: String? = nil
+    public var lat: Double? = nil
+    public var lon: Double? = nil
+    public var tagline: String? = nil
+    public var hasCoupon = false
+    public var location: String? = nil
+
+
+    public var description: String{
+        // 下記は読み取り専用
+        get{
+            var string = "\nGid: \(gid)\n"
+            string += "Name: \(name)\n"
+            string += "PhotoUrl: \(photoUrl)\n"
+            string += "Yomi: \(yomi)\n"
+            string += "Tel: \(tel)\n"
+            string += "Address: \(address)\n"
+            string += "Lat & Lon \(lat), \(lon))\n"
+            string += "tagline: \(tagline)\n"
+            string += "hasCoupon:\(hasCoupon)\n"
+            string += "Location: \(location)\n"
+            return string
+        }
+    }
+}
+
+// added struct "Shop" in YelpLocal.swift
