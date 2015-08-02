f44e9649158a6f64ae5561aa84b48ce0cecada64
niixnoughtnothing <ryoh.t778@gmail.com>
2015-08-02 14:01:32 +0900

    Create FreewordTableViewCell

---
+++ GourmetSearchApp/FreewordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FreewordTableViewCell.swift
+//  GourmetSearchApp
+//
+//  Created by nixnoughtnothing on 8/2/15.
+//  Copyright (c) 2015 Sttir Inc. All rights reserved.
+//
+
+import UIKit
+
+class FreewordTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var freeword: UITextField!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
