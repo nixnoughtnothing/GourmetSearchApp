790ca5a0fff254af850fd35095ab3a92991c6790
niixnoughtnothing <ryoh.t778@gmail.com>
2015-08-07 22:45:14 +0900

    Create ShopDetailViewController.swift

---
+++ GourmetSearchApp/ShopList/ShopDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ShopDetailViewController.swift
+//  GourmetSearchApp
+//
+//  Created by nixnoughtnothing on 8/7/15.
+//  Copyright (c) 2015 Sttir Inc. All rights reserved.
+//
+
+import UIKit
+
+class ShopDetailViewController: UIViewController {
+
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
